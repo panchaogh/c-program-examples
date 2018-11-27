9d5e21949ec069cd6c479249a2878b76e7d93323
panchao <panchao@xmjr.com>
2018-11-27 14:27:56 +0800

    add

---
+++ Basics/9_3.c
@@ -0,0 +1,36 @@
+//
+// Created by panchao on 18-11-27.
+//
+#include <stdio.h>
+
+float sum(int m);
+
+/**
+ * 计算1-1/2+1/3-1/4+...+1/m的值
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[]) {
+    int m;
+    float result;
+    printf("请输入m的值(正整数)：");
+    scanf("%d", &m);
+    result = sum(m);
+    printf("1-1/2+1/3-1/4+...+1/%d=%f\n", m, result);
+    return 0;
+}
+
+/**
+ * 计算1-1/2+1/3-1/4+...+1/m的值
+ * @param m
+ * @return
+ */
+float sum(int m) {
+    float t = 1, result = 1.0;
+    for (int i = 2; i <= m; ++i) {
+        t = (-1) * t;
+        result += t * 1.0 / i;
+    }
+    return result;
+}
---
+++ Basics/9_4.c
@@ -0,0 +1,34 @@
+//
+// Created by panchao on 18-11-27.
+//
+
+/**
+ * 利用递归法求反序的输出
+ * @param x
+ */
+#include <stdio.h>
+
+void reverse(int x);
+
+int main(int argc, char *argv[]) {
+    int num;
+    printf("请输入一个整数（不大于五位数）：");
+    scanf("%d", &num);
+    if (num > 99999) {
+        printf("%d超过五位数！\n", num);
+        return 1;
+    }
+    printf("%d反序输出的结果为：", num);
+    reverse(num);
+    printf("\n");
+    return 0;
+}
+
+void reverse(int x) {
+    int num;
+    printf("%d", x % 10);
+    num = x / 10;
+    if (num) {
+        reverse(num);
+    }
+}
